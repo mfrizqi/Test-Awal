07b54ac33261808e5dfb24a4208da56d1e68732d
mfrizqi <muhammadfajarrizqi@gmail.com>
2017-05-01 20:44:14 +0700

    upload ajah

---
+++ Mahasiswa.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Muhammad Fajar Rizqi
+ */
+public class Mahasiswa {
+    
+    String nama;
+    String nim;
+    String kelas;
+    String fakultas;
+    int mmr;
+    int index_kebahagiaan;
+    
+    public Mahasiswa(String nama){
+        this.nama = nama;
+    }    
+    
+}
---
+++ testing/Testing.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testing;
+
+/**
+ *
+ * @author Muhammad Fajar Rizqi
+ */
+public class Testing extends Mahasiswa {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Mahasiswa m;
+        m = new Mahasiswa("Motion");
+        System.out.println(m.nama);
+    }
+    
+}
